fdae2661f6dcd2bf19d1c2e828126036666d8759
K2317842 <s4929892@bournemouth.ac.uk>
2025-01-07 12:35:25 +0000

    resolved merge conflicts

--- Assets/Scripts/Enemy/Boss/AttackStates/BaseAttackState.cs
+++ Assets/Scripts/Enemy/Boss/AttackStates/BaseAttackState.cs
@@ -30,27 +30,86 @@ namespace NPC
         protected string abilityName;
         [SerializeField]
         protected float cooldown;
+        [SerializeField]
+        protected bool comboAttack;
+        [SerializeField]
+        protected int numbOfCombo;
         private float lastUsedTime = -Mathf.Infinity;
         [SerializeField]
         protected float damage;
 
+        protected bool isActive =false;
+
+        protected int abilityUsage = 0;
+
+        private bool cooldownFinished = false;
+
+
         public override BossAIState stateTick(BossAIManager bossAI)
         {
             BossAIState stateToReturn = this;
+            checkIfCooldownNeedReset(stateToReturn);
             return stateToReturn;
+        }
+
+        protected void checkIfCooldownNeedReset(BossAIState stateToReturn)
+        {
+            if (stateToReturn != this)
+            {
+                BaseAttackState attackState = stateToReturn as BaseAttackState;
+
+                if (attackState != null && attackState.abilityName != this.abilityName)
+                {
+                    attackState.resetCooldown();
+                }
+            }
         }
 
         public abstract void Activate();
         protected abstract void visualizeAbility();
 
+        public abstract void resetValues();
+
         protected void startCooldown()
         {
+            Debug.Log($"Cooldown started for {abilityName}, duration: {cooldown}");
             lastUsedTime = Time.time;
+            abilityUsage++;
+            cooldownFinished = false;
         }
 
         protected bool isOnCooldown()
         {
             return Time.time < (lastUsedTime + cooldown);
+        }
+
+        protected bool checkCooldownStateChange()
+        {
+            if (cooldown == 0)
+            {
+                cooldownFinished = true;
+                return true;
+            }
+
+            if (!isOnCooldown() && !cooldownFinished)
+            {
+                cooldownFinished = true;
+                return true;
+            }
+            return false;
+        }
+
+        public void resetCooldown()
+        {
+            isActive = false;
+            abilityUsage = 0;
+            lastUsedTime = -Mathf.Infinity;
+        }
+
+        //getters
+        public string getName()
+        {
+            return abilityName;
         }
 
 
---
+++ Assets/Scripts/Enemy/Boss/AttackStates/BlightedPounce.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+namespace NPC
+{
+    [CreateAssetMenu(menuName = "AI/States/Attacks/Pounces")]
+    public class BlightedPounce : BaseAttackState
+    {
+        Transform bossTransform;
+        [SerializeField] private float leapSpeed = 10f;
+        [SerializeField] private float leapDistance = 5f; 
+        [SerializeField] private float impactRadius = 2f; 
+        [SerializeField] private LayerMask targetLayer;
+        Vector3 targetPosition = Vector3.zero;
+        Transform playerPos;
+
+        public override BossAIState stateTick(BossAIManager bossAI)
+        {
+            BossAIState stateToReturn = this;
+            bossTransform = bossAI.getBossTransform();
+            if (!isOnCooldown()&& !isActive)
+            {
+                playerPos = bossAI.getSetCurrentTarget;
+                bossAI.getSetCurrentTarget = null;
+                Activate();
+
+            }
+
+            if(isActive)
+            {
+                LeapTowardsTarget();
+                checkLeapEnd();
+                visualizeAbility();
+            }
+            if (checkCooldownStateChange())
+            {
+                Debug.Log("cooldown done pounce");
+                stateToReturn = nextState == null ? stateToReturn : nextState;
+            }
+            checkIfCooldownNeedReset(stateToReturn);
+            return stateToReturn;
+        }
+
+        public override void Activate()
+        {
+            Debug.Log($"{abilityName} activated!");
+            if(playerPos)
+            {
+                targetPosition = playerPos.position;
+                isActive = true;
+            }
+            else
+            {
+                targetPosition = Vector3.zero;
+            }
+
+        }
+
+        public override void resetValues()
+        {
+            isActive = false;
+            targetPosition = Vector3.zero;
+            resetCooldown();
+        }
+
+        private void LeapTowardsTarget()
+        {
+            if (targetPosition == Vector3.zero)
+            {
+                Debug.LogWarning("Player position is null!");
+                return;
+            }
+            Vector3 direction = (targetPosition - bossTransform.position).normalized;
+            Vector3 leapMovement = direction * leapSpeed * Time.deltaTime;
+            bossTransform.position = Vector3.MoveTowards(bossTransform.position, targetPosition, leapMovement.magnitude);
+        }
+
+        private void checkLeapEnd()
+        {
+            Vector3 bossPositionXZ = new Vector3(bossTransform.position.x, 0, bossTransform.position.z);
+            Vector3 targetPositionXZ = new Vector3(targetPosition.x, 0, targetPosition.z);
+
+            if (Vector3.Distance(bossPositionXZ, targetPositionXZ) <= impactRadius)
+            {
+                isActive = false;
+                checkImpact();
+                
+            }
+        }
+
+        private void checkImpact()
+        {
+            Collider[] hitTargets = Physics.OverlapSphere(bossTransform.position, impactRadius * 2f, targetLayer);
+            foreach (var hit in hitTargets)
+            {
+                if (hit.CompareTag("Player"))
+                {
+                    // TODO Apply high damage to Ilyra
+                    Debug.Log("Ilyra hit! Taking high damage!");
+                    // TODO add knockback or other effects here
+                    
+                }
+            }
+            startCooldown();
+        }
+
+        protected override void visualizeAbility()
+        {
+            //TODO set animation
+
+        }
+    }
+}
